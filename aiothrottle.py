562998e0215cae0d5759db92fd153c7b5618afe3
Sebastian Hüther <sebastian.huether@gmx.de>
2015-07-28 15:30:26 +0200

    Trying out asyncio.Condition as notification for new possible data reading; Renamed byte_limit to rate_limit; Passing through the buffer size limit

--- aiothrottle.py
+++ aiothrottle.py
@@ -3,11 +3,12 @@
 import asyncio
 import time
 import logging
+from asyncio import Condition
 
 class Throttle:
     """Throttle for an asnycio stream"""
 
-    def __init__(self, base_stream, byte_limit, interval=1.0):
+    def __init__(self, base_stream, rate_limit, interval=1.0):
         """
         :param base_stream: the asyncio stream to be throttled
         :type: asyncio.StreamReader, asyncio.StreamWriter
@@ -23,21 +24,37 @@ class Throttle:
         if interval <= 0.0:
             raise ValueError("interval must be positive")
 
-        self.byte_limit = byte_limit
+        self.rate_limit = rate_limit
         self.interval = interval
         self._base_stream = base_stream
         self._interv_start = 0
         self._io_in_interv = 0
+        self._new_interval_cond = Condition()
 
-    def _check_interval(self):
+    @asyncio.coroutine
+    def _new_interval(self):
+        """notifies a waiting call that a new interval has started
+
+        :rtype: None
+        """
+        with (yield from self._new_interval_cond):
+            self._new_interval_cond.notify()
+
+    def _reset_interval(self, timestamp):
+        self._io_in_interv = 0
+        self._interv_start = timestamp
+        asyncio.async(self._new_interval())
+
+    def check_interval(self):
         """checks if the current interval has passed and a new one has started
 
+        This has to be called when new data is available, so that waiting
+        calls get notified if the interval has passed
         :rtype: None
         """
         now = time.time()
         if now - self._interv_start > self.interval:
-            self._io_in_interv = 0
-            self._interv_start = now
+            self._reset_interval(now)
 
     def allowed_io(self):
         """checks if a requested IO action is allowed
@@ -45,8 +62,8 @@ class Throttle:
         :returns: number of bytes allowed to read/write
         :rtype: int
         """
-        self._check_interval()
-        return self.byte_limit - self._io_in_interv
+        self.check_interval()
+        return self.rate_limit - self._io_in_interv
 
     def add_io(self, byte_count):
         """registers a number of bytes read/written
@@ -58,9 +75,9 @@ class Throttle:
         self._io_in_interv += byte_count
 
 class ThrottledStreamReader(asyncio.StreamReader):
-    def __init__(self, base_stream, limit, interval=1.0):
-        super().__init__()
-        self._throttle = Throttle(base_stream, limit, interval)
+    def __init__(self, base_stream, rate_limit, interval=1.0, buf_limit=2**16):
+        super().__init__(limit=buf_limit)
+        self._throttle = Throttle(base_stream, rate_limit, interval)
 
 class TestReadTransport(asyncio.ReadTransport):
     # default limit: 1 GB
@@ -133,7 +150,7 @@ def run_reader_test():
         closed_waiter.set_result(None)
 
     base_reader = asyncio.StreamReader()
-    reader = ThrottledStreamReader(base_reader, limit=10)
+    reader = ThrottledStreamReader(base_reader, rate_limit=10)
 
     protocol = asyncio.StreamReaderProtocol(reader)
 
