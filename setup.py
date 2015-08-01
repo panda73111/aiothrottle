26cf94dfced1ca7e884cbdbd429c93e7c4acd345
Sebastian Hüther <sebastian.huether@gmx.de>
2015-08-01 23:05:31 +0200

    Fixed classifier in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
