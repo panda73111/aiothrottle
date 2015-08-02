e364be967fa2ee421713e71896a862f7ecabbf71
Sebastian Hüther <sebastian.huether@gmx.de>
2015-08-02 16:42:49 +0200

    Forgot importing limit_rate() and unlimit_rate() in __init__.py

--- aiothrottle/__init__.py
+++ aiothrottle/__init__.py
@@ -2,7 +2,7 @@
 Providing classes for limiting data rates of asyncio sockets
 """
 
-from .throttle import Throttle, ThrottledStreamReader
+from .throttle import Throttle, ThrottledStreamReader, limit_rate, unlimit_rate
 
 __version__ = "0.1.0"
 
