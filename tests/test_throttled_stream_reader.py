b2c03855a28ce875fe02f29f026957dcddad4bda
Sebastian Hüther <sebastian.huether@gmx.de>
2015-08-04 21:15:44 +0200

    Working basic data read tests for ThrottledStreamReader

--- tests/test_throttled_stream_reader.py
+++ tests/test_throttled_stream_reader.py
@@ -16,9 +16,11 @@ class TestThrottledStreamReader(unittest.TestCase):
         self.loop.close()
 
     def _make_one(self, *args, **kwargs):
-        return aiothrottle.ThrottledStreamReader(
+        r = aiothrottle.ThrottledStreamReader(
             self.stream, rate_limit=10, buffer_limit=1,
             loop=self.loop, *args, **kwargs)
+        self.assertTrue(self.transp.resume_reading.called)
+        return r
 
     def test_parameters(self):
         r = self._make_one()
@@ -38,7 +40,6 @@ class TestThrottledStreamReader(unittest.TestCase):
         with mock.patch.object(self.loop, "time", return_value=112):
             res = self.loop.run_until_complete(r.read(1))
         self.assertEqual(res, b'd')
-        # self.assertTrue(self.transp.resume_reading.called)
 
     def test_readline(self):
         with mock.patch.object(self.loop, "time", return_value=111):
@@ -47,7 +48,6 @@ class TestThrottledStreamReader(unittest.TestCase):
         with mock.patch.object(self.loop, "time", return_value=112):
             res = self.loop.run_until_complete(r.readline())
         self.assertEqual(res, b'data\n')
-        # self.assertTrue(self.transp.resume_reading.called)
 
     def test_readany(self):
         with mock.patch.object(self.loop, "time", return_value=111):
@@ -56,7 +56,6 @@ class TestThrottledStreamReader(unittest.TestCase):
         with mock.patch.object(self.loop, "time", return_value=112):
             res = self.loop.run_until_complete(r.readany())
         self.assertEqual(res, b'data')
-        # self.assertTrue(self.transp.resume_reading.called)
 
     def test_readexactly(self):
         with mock.patch.object(self.loop, "time", return_value=111):
@@ -65,10 +64,9 @@ class TestThrottledStreamReader(unittest.TestCase):
         with mock.patch.object(self.loop, "time", return_value=112):
             res = self.loop.run_until_complete(r.readexactly(2))
         self.assertEqual(res, b'da')
-        # self.assertTrue(self.transp.resume_reading.called)
 
     def test_feed_data(self):
         r = self._make_one()
         self.stream.paused = False
         r.feed_data(b'datadata', 8)
-        # self.assertTrue(self.transp.pause_reading.called)
+        self.assertTrue(self.transp.pause_reading.called)
