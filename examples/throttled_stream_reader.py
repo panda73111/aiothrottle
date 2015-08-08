b58f40fc0232084863fa213947524fc5c676d4fc
Sebastian Hüther <sebastian.huether@gmx.de>
2015-08-08 15:33:28 +0200

    Added interval rate printing to example

--- examples/throttled_stream_reader.py
+++ examples/throttled_stream_reader.py
@@ -17,18 +17,28 @@ def load_file(url, loop):
         read_next = True
         loaded = 0
         prev_progress = -1
+        prev_print_time = start_time
+        prev_print_loaded = 0
         while read_next:
             # read 1 MB chunks
             chunk = yield from response.content.read(2**20)
             file.write(chunk)
 
-            progress = int(loaded * 100 / size)
-            if progress != prev_progress:
-                print("%d%%" % progress)
-            prev_progress = progress
-
             chunk_len = len(chunk)
             loaded += chunk_len
+
+            progress = int(loaded * 100 / size)
+            if progress and progress != prev_progress:
+                # print progress and current download rate
+                now = loop.time()
+                period = now - prev_print_time
+                loaded_since = loaded - prev_print_loaded
+                temp_rate = loaded_since / period
+                print("%d%% - %d KB/s" % (progress, temp_rate / 1024))
+                prev_print_time = now
+                prev_print_loaded = loaded
+            prev_progress = progress
+
             read_next = chunk_len != 0
     response.close()
 
@@ -47,7 +57,7 @@ def main():
     # download a large file without blocking bandwidth
     loop = asyncio.get_event_loop()
     loop.run_until_complete(load_file(
-        "http://ipv4.download.thinkbroadband.com/5MB.zip", loop))
+        "http://ipv4.download.thinkbroadband.com/10MB.zip", loop))
 
     # unset the rate limit
     aiothrottle.unlimit_rate()
